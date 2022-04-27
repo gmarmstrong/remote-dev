c38034e880237dd8624a18536477fccee2edb65d
Guthrie McAfee Armstrong <guthrie.armstrong@gmail.com>
2022-04-27 00:30:43 -0400

    ci: fix typo causing config_arg error

--- tflints.sh
+++ tflints.sh
@@ -7,8 +7,11 @@ set -eo pipefail
 terraform --version
 tflint --version
 
-config_file="$(git rev-parse --show-toplevel)/terraform/.tflint.hcl"
-if [[ -f "$config_file" ]]; then # if root tflint config exists
+cd terraform
+
+# check for global tflint config
+config_file="${PWD}/.tflint.hcl"
+if [[ -f "$config_file" ]]; then
   config_arg="--config=$config_file"
 fi
 
@@ -30,10 +33,10 @@ for module_dir in "${module_dirs[@]}"; do
       for profile in "${profiles[@]}"; do
         # profiles are .tfvars files, configs are .tflint.hcl files
         if [[ ".tflint.hcl" ]]; then
-          if [[ config_arg ]]; then
-            echo "Warning: found both root and local tflint configs. Using the local one."
+          if [[ config_arg != "" ]]; then
+            echo "Warning: found both global and local tflint configs. Using the local one."
           fi
-          config_arg="--$config_file"
+          config_arg="--config=$config_file"
         fi
         tflint --var-file="$profile" "$config_arg"
       done
